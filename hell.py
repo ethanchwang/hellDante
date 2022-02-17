9cf8309516469bb78c49e087a7a6210f02d438ad
ethanchwang <ethanhwang2@gmail.com>
2022-02-17 02:22:02 -0500

    update csv, compare function

--- hell.py
+++ hell.py
@@ -2,36 +2,40 @@ import gensim
 from gensim import models
 from gensim.models import Word2Vec
 import numpy as np
+import csv
 
 def csvToList(path):
-    my_file = open(path, "r")
-    newList = my_file.read().split("\n")
-    my_file.close()
+    with open(path, newline='') as f:
+        reader = csv.reader(f)
+        newList = list(reader)
+    for _ in range(0,len(newList)):
+        newList[_][1] = int(newList[_][1])
     return newList
+
+def getSimilarity(confession, comparison):
+    similarity = []
+    for j in range(0,len(comparison)):
+        similarity.append([])
+        for i in range(0,len(confession)):
+            similarity[j].append(0)
+            try:
+                similarity[j][i] = (model.similarity(confession[i], comparison[j][0]))*pow(comparison[j][1],.5)
+            except:
+                pass
+    print(comparison)
+    print(similarity)
+    return similarity
 
 model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True) #without *norm_only* param
 
-print(model.similarity('eagle', 'python'))
-# print(model.most_similar(positive=['unbaptized']))
-
-uq1 = csvToList('/Users/ethanhwang/Documents/hellDante/uq1.csv')
+uq1 = csvToList('/Users/ethanhwang/Documents/hellDante/UQcircle1.csv')
 
 similarity1 = []
 
 print("""confess now!""")
 confession = input()
 confessionList = confession.split()
-for j in range(0,len(uq1)):
-    similarity1.append([])
-    for i in range(0,len(confessionList)):
-        similarity1[j].append(0)
-        try:
-            similarity1[j][i] = model.similarity(confessionList[i], uq1[j])
-        except:
-            pass
-
-print(similarity1)
-print(len(similarity1))
+similarity1 = getSimilarity(confessionList, uq1)
 
 closestSimilarity1 = np.argmax(similarity1, axis=0)
 print(closestSimilarity1)
